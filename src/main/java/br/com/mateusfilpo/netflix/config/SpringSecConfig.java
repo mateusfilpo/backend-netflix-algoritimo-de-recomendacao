81f538db47a5100826348f2cc41d02c5d2381203
Mateus Filpo <mateusfilpo27@gmail.com>
2024-11-23 18:18:30 -0300

    feat: adicionar configuração de OAuth2 Resource Server com proteção de endpoints

---
+++ src/main/java/br/com/mateusfilpo/netflix/config/SpringSecConfig.java
@@ -0,0 +1,24 @@
+package br.com.mateusfilpo.netflix.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
+
+        return http.build();
+    }
+
+}
